89afe0fea1d3938732af38a444f0780a5d81a472
jordiZ1 <58164856+jordiZ1@users.noreply.github.com>
2020-03-20 15:04:36 +0100

    derdepoging
    test

--- src/teamB/Test.java
+++ src/teamB/Test.java
@@ -5,6 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("team B test !");
+		System.out.print("test 2");
+		System.out.print("Testhdiui");
+		System.out.print("tetduyedy");
+		System.out.print("tuyeigfstfgusyegfiusgiuidvhjvhjftydtydtydtydtuydyu");
+		System.out.println("yifrgayugayuegfusfgyueogf");
+		System.out.println("testttetstg");
 	}
 
 }
