300aedcab1637448c4b19f2e71ca7183eebd56b8
jordi <jordi@192.168.56.1>
2020-03-28 15:02:19 +0100

    quizes 5/6

---
+++ src/hoofdstuk10/Quiz6.java
@@ -0,0 +1,38 @@
+
+package hoofdstuk10;
+
+/**
+ * @author jordi
+ *
+ */
+public class Quiz6 {
+	
+	void myMethod() {
+		String strArray[] = {"n","b","a","z"};
+		
+		for(int i = 0; i < strArray.length; i ++) {
+			if(strArray[i].equals("z")) {
+				System.out.print("x1 ");
+			}
+			else if(strArray[i].equals("a")) {
+				System.out.print("x2 ");
+			}
+			else if(strArray[i].equals("b")) {
+				System.out.print("x2 ");
+			}
+			else {
+				System.out.print("x3 ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Quiz6 mk = new Quiz6();
+		mk.myMethod();
+		
+		//door de hoofdletter B te vervangen door een kleine letter b wordt er x3 x2 x2 x1 geprint
+
+	}
+
+}
