2bbe5c1282ab5bc01b3f7184149b564110b1b162
Jordi <jordizweden@gmail.com>
2020-05-29 14:30:08 +0200

    jordi

---
+++ src/hoofdstuk13/Quiz2_super.java
@@ -0,0 +1,15 @@
+package hoofdstuk13;
+
+public class Quiz2_super {
+	
+	final void methodeA() {
+		System.out.print("x");
+	}
+	final void methodeB(int i) {
+		System.out.print("x" + i);
+	}
+	final void methodeC(String str) {
+		System.out.print("x");
+	}
+
+}
