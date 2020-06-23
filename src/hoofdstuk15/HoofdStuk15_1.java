e2ebe4cefbaeee1a35f7896c6efcf694f3c5a0c7
noel0 <noel0@10.150.140.31>
2020-06-23 12:51:14 +0200

    Hallo meneer mijn opdracht - nOEL

--- src/hoofdstuk15/HoofdStuk15_1.java
+++ src/hoofdstuk15/HoofdStuk15_1.java
@@ -1,20 +1,9 @@
-/**
+7/**
  * 
  */
 package hoofdstuk15;
 
-/**
- * @author noel0
- *
- */
-public abstract class HoofdStuk15_1 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Televisie implements ArtikelInterface
+{
+	double prijs = 420;
 }
