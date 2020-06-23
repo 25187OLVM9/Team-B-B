02c0b1c1a3ad4cff2bb2239fcf8c60c57c85f72f
noel0 <noel0@10.150.140.31>
2020-06-23 12:50:44 +0200

    Kapotje mooi - nOËL

---
+++ src/hoofdstuk15/HoofdStuk15_1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hoofdstuk15;
+
+/**
+ * @author noel0
+ *
+ */
+public abstract class HoofdStuk15_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
